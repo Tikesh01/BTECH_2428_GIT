aa0aedecc4466615ab111320f538f6d0c5d74c94
Tikesh sahu <ts2665857@gmail.com>
2026-06-19 07:01:57 +0530

    C Codes

---
+++ C/B_TECH/printing.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    printf("*\n");
+    for(i=0; i<2; i++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+    for(i=0; i<3; i++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+    for(i=0; i<4; i++)
+    {
+        printf("* ");
+    }
+
+    // int i;
+    // printf("\t\t1\n\n");
+    // for(i=0; i<2; i++){
+    //     printf("\t2\t");
+    // }
+    // printf("\n\n");
+    // for (i = 0; i <3; i++)
+    // {
+    //     printf("3\t\t");
+    // }
+    
+    return 0;
+    
+}
