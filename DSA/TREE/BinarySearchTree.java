469188c500f8880e9ff7ab2688051c37c8d03f6c
Tikesh sahu <ts2665857@gmail.com>
2026-07-07 18:45:39 +0530

    Add Inorder Traversal and exixtance of value implementation in Binary Search Tree

--- DSA/TREE/BinarySearchTree.java
+++ DSA/TREE/BinarySearchTree.java
@@ -12,19 +12,11 @@ class Node {
 }
 
 
-public class BinarySearchTree {
-    void main() {
-        int[] arr = {3, 2, 1, 4, 5, 6};
-        Node root = createBST(arr);
-        System.out.print(root.right.val);
-    }
+class BinSearchTree {
+    Node root;
 
-    Node createBST(int[] arr) {
-        Node root = null;
-        for (int v : arr) {
-            root = this.insert(root, v);
-        }
-        return root;
+    BinSearchTree() {
+        root = null;
     }
 
     Node insert(Node root, int v) {
@@ -40,4 +32,54 @@ public class BinarySearchTree {
 
         return root;
     }
+
+    BinSearchTree(int[] arr) {
+        this.root = null;
+        for (int v : arr) {
+            this.root = this.insert(root, v);
+        }
+    }
+
+    boolean exist = false;
+
+    public boolean exists(Node temp, int val) {
+        if (temp == null) {
+            return false;
+        }
+        if (temp.val == val) {
+            return true;
+        } else if (temp.val > val) {
+            exist = exists(temp.left, val);
+        } else {
+            exist = exists(temp.right, val);
+        }
+
+        return exist;
+    }
+
+    // gives sorted arrays as result always
+    public void inorderTravesal(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorderTravesal(root.left);
+        System.out.print(root.val + " ");
+        inorderTravesal(root.right);
+    }
+}
+
+
+public class BinarySearchTree {
+    void main() {
+        int[] arr = {3, 2, 1, 4, 5, 6};
+        BinSearchTree bst = new BinSearchTree(arr);
+        bst.inorderTravesal(bst.root);
+        System.out.println(bst.exists(bst.root, 4));
+        System.out.println(bst.exists(bst.root, 0));
+        System.out.println(bst.exists(bst.root, 1));
+        System.out.println(bst.exists(bst.root, 6));
+
+    }
+
+
 }
