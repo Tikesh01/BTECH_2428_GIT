759f6e5f5d5495c5f9d4279934eeb7e12fa2f466
Tikesh sahu <ts2665857@gmail.com>
2026-07-09 14:54:26 +0530

    sfa

---
+++ C/Codes/Loops/for/tableof19.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int i,t;
+    // for (i = 1; i <= 200; i++)
+    // {
+    //     if (i % 19 == 0)
+    //     {
+    //         printf("%d ", i);
+    //     }
+    // }
+    for ( i = 1; i <=10; i++)
+    {
+        t=i*19;
+        printf("\n%d",t);
+    }
+    
+        // }
+        // OR
+        // for ( i = 19; i < 200; i=i+19)
+        // {
+        //      printf("%d ",i);
+        // }
+
+        return 0;
+}
