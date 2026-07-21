cb3ee1c81b8e4530111120ccde1a191afaf8a48a
Tikesh sahu <ts2665857@gmail.com>
2026-07-21 21:25:52 +0530

    String in C

---
+++ C/Codes/String/stringh.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include<stdlib.h>
+
+int main()
+{
+    // char str[]="Hor are you";
+    // int n = strlen(str);
+    // printf("%d ",n);
+    // // printf("%c",str[10]);
+    // strcpy(str,"hii");
+
+
+    // char ch[]=" 493441";
+    // char city[]=" raipur";
+    // strcat(ch,city);
+    // puts(ch);
+
+
+    // char vill[]="birbira";
+    // char viil[]="birbira";
+    // int f=strcmp(vill,viil);
+    // printf("%d",f);
+
+
+    char ch[]="12893r";
+    int n =  atoi(ch);
+    printf("%d ",n);
+}
