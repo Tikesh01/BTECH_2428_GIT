73d12a59f50cd412fb45ac7a1282f6f3e8acf5b0
Tikesh sahu <ts2665857@gmail.com>
2026-07-25 16:58:33 +0530

    Nested Loop in C

---
+++ C/Codes/Nested Loop/PrintNumTriangle.c
@@ -0,0 +1,36 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int i, n, j;
+//     printf("Enter the number : ");
+//     scanf("%d", &n);
+//     for (i = 1; i <= n; i++)
+//     {
+//         for (j = 1; j <= i; j++)
+//         {
+//             printf("%d  ",j);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// OPPOSITE TRIANGLE
+
+#include <stdio.h>
+int main()
+{
+    int i, n, j;
+    printf("Enter the number : ");
+    scanf("%d", &n);
+    for (i = 1; i <= n;)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            printf("%d  ",j);
+        }
+        n--;
+        printf("\n");
+    }
+    return 0;
+}
