ed0a45b689b4b9a94f0c7368da495a333a2eb38d
Tikesh01 <ts2665857@gmail.com>
2026-08-02 21:17:38 +0530

    Ascii

---
+++ C/Codes/ASCII.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+// American Standard cade for information interchange: 
+int main(){
+    char ch= 'a';
+    char ch2='b';
+    char ch3='c';
+    char ch4='d';
+    char ch5='e';
+    char ch6='f';
+    char ch7='g';
+    char ch12='l';
+    printf("%d %d %d %d %d %d %d %d  ",ch,ch2,ch3,ch4,ch5,ch6,ch7,ch12);
+}
