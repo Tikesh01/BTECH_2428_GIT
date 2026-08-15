aacbefd83056292cdde4b1edfb386689295c2810
Tikesh01 <ts2665857@gmail.com>
2026-08-15 18:24:06 +0530

    Basic func definition

---
+++ C/Kanetkar/msg.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// int msg()
+// {
+//     printf("hii");
+//     return;
+// }
+// int main()
+// {
+//     int a;
+//     a = msg();
+// }
+int main()
+{
+    SQR();
+    int a;
+    {
+        return (a*a);
+    }
+}
