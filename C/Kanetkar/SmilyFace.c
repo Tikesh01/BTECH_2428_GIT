2ced1b71b5997bc7a69d5bf8df226518e6c68904
Tikesh01 <ts2665857@gmail.com>
2026-08-26 21:18:18 +0530

    c

---
+++ C/Kanetkar/SmilyFace.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    for (int i = 0; i <= 50; i++)
+    {
+        for (int j = 0; j <= 89; j++)
+        {
+            printf("%c ", (char)1);
+        }
+        printf("\n");
+    }
+
+    getchar();
+    return 0;
+}
